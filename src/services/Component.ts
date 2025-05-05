cffe0bdbf8b4acdf3493c8fdd5f6c14b5be3f70e
mkotelnikov <maksim.kotelnikov@soft-control.ru>
2025-05-05 20:16:26 +0500

    fix: fix types for Component and EventBus

--- src/services/Component.ts
+++ src/services/Component.ts
@@ -1,103 +1,260 @@
+import { makeUUID } from '../utils/makeUUID';
 import { deepEqual } from '../utils/deepEqual';
-import { ObjectI } from '../utils/types';
+import Handlebars from 'handlebars';
 import { EventBus } from './EventBus';
 
-export class Component {
+export interface BaseProps {
+  settings?: {
+    withInternalID?: boolean;
+  };
+  events?: Record<string, EventListener>;
+  attrs?: Record<string, string>;
+}
+
+export type ComponentChildren<C extends Record<string, Component<any, any>>> =
+  C;
+
+interface ComponentEvents<P> extends Record<string, unknown> {
+  init: [];
+  'flow:component-did-mount': [];
+  'flow:component-did-update': [
+    oldP: P & { _id: string },
+    newP: P & { _id: string }
+  ];
+  'flow:render': [];
+}
+
+export class Component<
+  P extends BaseProps = {},
+  C extends Record<string, Component<any, any>> = {}
+> {
   static EVENTS = {
     INIT: 'init',
-    FLOW_CDM: 'flow:component-did-mount',
-    FLOW_CDU: 'flow:component-did-update',
-    FLOW_RENDER: 'flow:render',
-  };
+    CDM: 'flow:component-did-mount',
+    CDU: 'flow:component-did-update',
+    RENDER: 'flow:render',
+  } as const;
 
-  _element?: HTMLElement;
-  _meta: { tagName: string; props: unknown };
-  eventBus: () => EventBus<{ [x: string]: unknown }>;
-  props: object;
+  private _props: P & { _id: string };
+  private _children: Partial<C>;
+  private _id: string;
+  private _element!: HTMLElement;
+  private _meta: { tag: string; props: P };
+  private _eventBus: EventBus<ComponentEvents<P>>;
+  private _setUpdate = false;
 
-  constructor(tagName = 'div', props = {}) {
-    const eventBus = new EventBus();
+  constructor(
+    tag: string = 'div',
+    propsAndChildren: P & Partial<{ [K in keyof C]: C[K] }> = {} as any
+  ) {
+    this._id = makeUUID();
+    const { children, props } = this.splitPropsAndChildren(propsAndChildren);
 
-    this._meta = {
-      tagName,
-      props,
+    this._eventBus = new EventBus<ComponentEvents<P>>();
+    this._children = this.createChildrenProxy(children);
+    this._props = this.createPropsProxy({ ...props, _id: this._id });
+    this._meta = { tag, props };
+
+    this.registerEvents();
+    this._eventBus.emit(Component.EVENTS.INIT);
+  }
+
+  private registerEvents(): void {
+    this._eventBus.on('init', this.init.bind(this));
+    this._eventBus.on(
+      'flow:component-did-mount',
+      this._componentDidMount.bind(this)
+    );
+    this._eventBus.on(
+      'flow:component-did-update',
+      this._componentDidUpdate.bind(this)
+    );
+    this._eventBus.on('flow:render', this._render.bind(this));
+  }
+
+  private init(): void {
+    this._element = this.createDocumentElement(this._meta.tag);
+    this._eventBus.emit(Component.EVENTS.RENDER);
+  }
+
+  private createDocumentElement(tag: string): HTMLElement {
+    const el = document.createElement(tag);
+    if (this._props.settings?.withInternalID) {
+      el.setAttribute('data-id', this._id);
+    }
+    return el;
+  }
+
+  private _render(): void {
+    const html = this.render();
+    this.removeEvents();
+    this._element.innerHTML = html;
+    this.addAttributes();
+    this.addEvents();
+  }
+
+  protected render(): string {
+    return '';
+  }
+
+  private addEvents(): void {
+    for (const [evt, handler] of Object.entries(this._props.events ?? {})) {
+      this._element.addEventListener(evt, handler);
+    }
+  }
+
+  private removeEvents(): void {
+    for (const [evt, handler] of Object.entries(this._props.events ?? {})) {
+      this._element.removeEventListener(evt, handler);
+    }
+  }
+
+  private addAttributes(): void {
+    for (const [attr, val] of Object.entries(this._props.attrs ?? {})) {
+      this._element.setAttribute(attr, val);
+    }
+  }
+
+  private splitPropsAndChildren(input: P & Partial<{ [K in keyof C]: C[K] }>): {
+    props: P;
+    children: Partial<C>;
+  } {
+    const props: Partial<P> = {};
+    const children: Partial<C> = {};
+    for (const key in input) {
+      const val = (input as unknown as Record<string, unknown>)[key];
+      if (val instanceof Component) {
+        (children as unknown as Record<string, Component>)[key] = val;
+      } else {
+        (props as unknown as Record<string, unknown>)[key] = val;
+      }
+    }
+    return {
+      props: props as P,
+      children: children as Partial<C>,
     };
-
-    this.eventBus = () => eventBus;
-    this.props = this._makePropsProxy(props);
   }
 
-  private _createResources() {
-    const { tagName } = this._meta;
-    this._element = this._createDocumentElement(tagName);
-  }
+  protected compile(
+    template: string,
+    propsOverride?: Partial<P & { _id: string }>
+  ): DocumentFragment {
+    const context = { ...(propsOverride ?? this._props) } as Record<
+      string,
+      unknown
+    >;
+    for (const [key, child] of Object.entries(this._children)) {
+      context[key] = `<div data-id="${child._id}"></div>`;
+    }
 
-  init() {
-    this._createResources();
-    this.eventBus().emit(Component.EVENTS.FLOW_RENDER);
+    const tpl = Handlebars.compile(template)(context);
+    const tplEl = document.createElement('template');
+    tplEl.innerHTML = tpl;
+
+    for (const child of Object.values(this._children)) {
+      const stub = tplEl.content.querySelector(`[data-id="${child._id}"]`);
+      stub?.replaceWith(child.getContent());
+    }
+
+    return tplEl.content;
   }
 
   private _componentDidMount(): void {
     this.componentDidMount();
-  }
-
-  componentDidMount(oldProps?: unknown) {}
-
-  dispatchComponentDidMount(): void {
-    this.eventBus().emit(Component.EVENTS.FLOW_CDM);
-  }
-
-  private _componentDidUpdate(oldProps: ObjectI, newProps: Object) {
-    const response = this.componentDidUpdate(oldProps, newProps);
-    if (!response) {
-      return;
+    for (const child of Object.values(this._children)) {
+      child.dispatchComponentDidMount();
     }
-    this._render();
+  }
+  protected componentDidMount(): void {}
+
+  public dispatchComponentDidMount(): void {
+    this._eventBus.emit(Component.EVENTS.CDM);
+    if (Object.keys(this._children).length) {
+      this._eventBus.emit(Component.EVENTS.RENDER);
+    }
   }
 
-  componentDidUpdate(oldProps: Object, newProps: Object): boolean {
-    return deepEqual(oldProps, newProps);
+  private _componentDidUpdate(
+    oldP: P & { _id: string },
+    newP: P & { _id: string }
+  ): void {
+    if (this.componentDidUpdate(oldP, newP)) {
+      this._eventBus.emit(Component.EVENTS.RENDER);
+    }
+  }
+  protected componentDidUpdate(
+    oldP: P & { _id: string },
+    newP: P & { _id: string }
+  ): boolean {
+    return deepEqual(oldP, newP);
   }
 
-  //Сначала нужно реализовать методы  _componentDidUpdate, _render,
-  // _registerEvents(eventBus: ) {
-  //   eventBus.
-  // }
+  public show(): void {
+    this.getContent().style.display = 'block';
+  }
+  public hide(): void {
+    this.getContent().style.display = 'none';
+  }
+  public getContent(): HTMLElement {
+    return this._element;
+  }
 
-  _makePropsProxy(props: { [key: string]: unknown }) {
+  public setProps(nextProps: Partial<P>): void {
+    if (!nextProps) return;
+    this._setUpdate = false;
+    const old = { ...this._props };
+
+    const { props, children } = this.splitPropsAndChildren({
+      ...(nextProps as any),
+    });
+    Object.assign(this._props, props);
+    Object.assign(this._children, children as C);
+
+    if (this._setUpdate) {
+      this._eventBus.emit(Component.EVENTS.CDU, old, this._props);
+      this._setUpdate = false;
+    }
+  }
+
+  private createPropsProxy<T extends object>(props: T): T {
     const self = this;
 
     return new Proxy(props, {
-      get(target, prop: string) {
+      get(target: T, p: string | symbol): unknown {
+        const prop = p as keyof T;
         const value = target[prop];
-        return typeof value === 'function' ? value.bind(target) : value;
+
+        return typeof value === 'function'
+          ? (value as Function).bind(target)
+          : value;
       },
 
-      set(target, prop: string, value) {
-        target[prop] = value;
-
-        self.eventBus().emit(Component.EVENTS.FLOW_CDU, { ...target }, target);
+      set(target: T, p: string | symbol, value: unknown): boolean {
+        const prop = p as keyof T;
+        if (target[prop] !== value) {
+          target[prop] = value as T[typeof prop];
+          self._setUpdate = true;
+        }
         return true;
-      },
-
-      deleteProperty() {
-        throw new Error('Нет доступа');
       },
     });
   }
 
-  _render() {
-    const block = this.render();
-    // Этот небезопасный метод для упрощения логики
-    // Используйте шаблонизатор из npm или напишите свой безопасный
-    // Нужно не в строку компилировать (или делать это правильно),
-    // либо сразу в DOM-элементы возвращать из compile DOM-ноду
-    this._element.innerHTML = block;
-  }
-  render() {}
+  private createChildrenProxy<Ct extends object>(children: Ct): Ct {
+    const self = this;
 
-  _createDocumentElement(tagName: string) {
-    // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
-    return document.createElement(tagName);
+    return new Proxy(children, {
+      get(target: Ct, p: string | symbol): unknown {
+        const prop = p as keyof Ct;
+        return target[prop];
+      },
+
+      set(target: Ct, p: string | symbol, value: unknown): boolean {
+        const prop = p as keyof Ct;
+        target[prop] = value as Ct[typeof prop];
+        self._setUpdate = true;
+        return true;
+      },
+    });
   }
 }
--- src/services/EventBus.ts
+++ src/services/EventBus.ts
@@ -1,44 +1,43 @@
 export type Events = Record<string, unknown>;
-
-export type Listener<Data = any> = (...args: Data[]) => void;
+export type Listener<Data = any> = Data extends any[]
+  ? (...args: Data) => void
+  : (arg: Data) => void;
 
 export class EventBus<EventsData extends Events> {
-  private listeners: Map<keyof EventsData, Set<Listener>> = new Map();
+  private listeners = new Map<keyof EventsData, Set<Listener>>();
 
   on<Key extends keyof EventsData>(
     event: Key,
-    callback: (...args: Listener<EventsData[Key]>[]) => void
-  ) {
-    const eventListenersSet = this.listeners.get(event);
-
-    if (!eventListenersSet) {
+    callback: Listener<EventsData[Key]>
+  ): void {
+    const handlers = this.listeners.get(event);
+    if (!handlers) {
       this.listeners.set(event, new Set([callback]));
     } else {
-      eventListenersSet.add(callback);
+      handlers.add(callback);
     }
   }
 
   off<Key extends keyof EventsData>(
-    event: string,
-    callback: (...args: Listener<EventsData[Key]>[]) => void
-  ) {
-    const eventListenersSet = this.listeners.get(event);
-
-    if (!eventListenersSet) {
-      throw new Error(`No event ${event}`);
-    } else {
-      eventListenersSet.delete(callback);
+    event: Key,
+    callback: Listener<EventsData[Key]>
+  ): void {
+    const handlers = this.listeners.get(event);
+    if (!handlers) {
+      throw new Error(`No listeners for event "${String(event)}"`);
     }
+    handlers.delete(callback);
   }
 
   emit<Key extends keyof EventsData>(
-    event: string,
-    ...args: EventsData[Key][]
-  ) {
-    const eventListenersSet = this.listeners.get(event);
-
-    if (eventListenersSet) {
-      eventListenersSet.forEach((listener) => listener(...args));
+    event: Key,
+    ...args: EventsData[Key] extends unknown[]
+      ? EventsData[Key]
+      : [EventsData[Key]]
+  ): void {
+    const handlers = this.listeners.get(event);
+    if (handlers) {
+      handlers.forEach((fn) => fn.apply(undefined, args as unknown[]));
     }
   }
 }
